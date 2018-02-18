cbf30208214e5c2defd9ada0ea11568c6e93e15c
SHUVRO <>
2018-02-18 19:21:11 +0600

    initial commit

---
+++ UnitTestAndUITest/HomeVC.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UnitTestAndUITest
+//
+//  Created by SHUVRO on 2/18/18.
+//  Copyright © 2018 SHUVRO. All rights reserved.
+//
+
+import UIKit
+
+class HomeVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
