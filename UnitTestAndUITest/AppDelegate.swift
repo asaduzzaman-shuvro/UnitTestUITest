98da0bb051d1836013c4508f1decfd7d58ddaaf0
Asaduzzaman Shuvro <asaduzzaman.duet@gmail.com>
2018-02-18 19:51:47 +0600

    dd

--- UnitTestAndUITest/AppDelegate.swift
+++ UnitTestAndUITest/AppDelegate.swift
@@ -17,7 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         
-//        let label = UILabel(frame: <#T##CGRect#>)
+//        let label = UILabel(frame:sfdsf  sd sdf  <#T##CGRect#>)
         
         
         return true
