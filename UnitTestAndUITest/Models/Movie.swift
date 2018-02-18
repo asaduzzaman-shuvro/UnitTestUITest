b066d99785be135f6fdfbb44de06327f538618a6
Asaduzzaman Shuvro <asaduzzaman.duet@gmail.com>
2018-02-18 20:07:24 +0600

    Movie file added.

---
+++ UnitTestAndUITest/Models/Movie.swift
@@ -0,0 +1,35 @@
+//
+//  Movie.swift
+//  UnitTestAndUITest
+//
+//  Created by SHUVRO on 2/18/18.
+//  Copyright © 2018 SHUVRO. All rights reserved.
+//
+
+import Foundation
+
+enum Genre {
+    case animation
+    case action
+    case none
+}
+
+struct Movie {
+    var title: String
+    var genre: Genre
+}
+
+class MovieHelper {
+    func getMovies() -> [Movie] {
+        return [
+            Movie(title: "The Emoji Movie", genre: .animation),
+            Movie(title: "Logan", genre: .action),
+            Movie(title: "Wonder Woman", genre: .action),
+            Movie(title: "Zootopia", genre: .animation),
+            Movie(title: "The Baby Boss", genre: .animation),
+            Movie(title: "Despicable Me 3", genre: .animation),
+            Movie(title: "Spiderman: Homecoming", genre: .action),
+            Movie(title: "Dunkirk", genre: .animation)
+        ]
+    }
+}
--- UnitTestAndUITest/ViewControllers/HomeVC.swift
+++ UnitTestAndUITest/ViewControllers/HomeVC.swift
@@ -13,7 +13,6 @@ class HomeVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        let view = UIView()
         
     }
 
